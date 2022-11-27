b4d8bb2ecf61b49f7e6bee0eae9add9e1a207cea
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-11-27 12:07:07 -0600

    update

--- app.js
+++ app.js
@@ -55,4 +55,21 @@ let data = [
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
+const circle = document.querySelector('#circle');
+
+circle.addEventListener('mouseenter', () => {
+  if (!circle.classList.contains('hover')) {
+    circle.classList.add('hover');
+  }
+});
+
+circle.addEventListener('mouseleave', () => {
+  if (circle.classList.contains('hover')) {
+    circle.classList.remove('hover');
+  }
+}); //                                                                         bubble
+////////////////////////////////////////////////////////////////////////////
+
+
+
 
