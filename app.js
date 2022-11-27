1233e72ac139dbf7baf0230ad6ccf7be35fbee71
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-11-27 12:01:45 -0600

    update

--- app.js
+++ app.js
@@ -14,8 +14,45 @@ openFace.addEventListener('click', () => {
     closedFace.classList.add('active');
     openFace.classList.remove('active');
   }
-});
+});       // Smile face 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let data = [
+    {
+      name: 'Batman',
+      age: '41'
+    },
+    {
+      name: 'WonderWoman',
+      age: '38'
+    },
+    {
+      name: 'SuperMan',
+      age: '43'
+    },
+    {
+      name: 'Flash',
+      age: '27'
+    },
+    {
+      name: 'GreenLantern',
+      age: '38'
+    },
+    {
+      name: 'Red Tornado',
+      age: '00'
+    }
+  ];
+  
+  const info = document.querySelector('#info');
+  
+  let details = data.map(function(item) {
+    return '<div>' + item.name + ' ' + 'is ' + item.age + ' years old' + '</div>';
+  });
+  
+  info.innerHTML = details.join('\n'); ///           names of justic crew
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
