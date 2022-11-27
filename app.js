d5cfe0b5b0a5eb8297e7e71b9f9cc62642d66e6f
Rgabrielart <70184276+Rgabrielart@users.noreply.github.com>
2022-11-27 12:29:04 -0600

    commit

--- app.js
+++ app.js
@@ -70,6 +70,40 @@ circle.addEventListener('mouseleave', () => {
 }); //                                                                         bubble
 ////////////////////////////////////////////////////////////////////////////
 
+const months = ['January', 'February', 'March', 'April']; // Array method
+
+months.forEach(function(month) {
+  console.log(month);
+});
+
+
+var dogs = Array("Bulldog", "Beagle", "Labrador");  // Array
+
+
+dogs[0] = "Bull Terier"; 
+console.log(dogs[1]);
+
+
+
+
+let person = {                       //  object method 
+  firstName: 'John',
+  lastName: 'Doe',
+  greet: function () {
+      console.log('IS this right?');
+  }
+};
+
+
+const persons = {                // object
+
+  name: "Gabe",
+  age: 40
+
+}
+console.log(persons); //                     object and array
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
